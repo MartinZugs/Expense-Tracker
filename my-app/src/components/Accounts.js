390af1778d0f38c9b3b178508fa8861dcea918ae
eethomas14 <ethomas14@luc.edu>
2021-04-08 16:58:26 -0500

    some more work on the pos and neg value coloring on the account and net worth components

--- my-app/src/components/Accounts.js
+++ my-app/src/components/Accounts.js
@@ -1,17 +1,18 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import ReactDOM from 'react-dom';
 import "./account.css";
 
 
 
-const Accounts = () => {
 
+const Accounts = () => {
     function MakePosNeg() {
         const TDs = document.querySelectorAll('.plusmin');
 
         for (let i = 0; i < TDs.length; i++) {
             let temp = TDs[i];
-            if (temp.firstChild.nodeValue.indexOf('-') === 0) {
-                temp.className = 'negative';
+            if (temp.firstChild.nodeValue.indexOf('-') == 0) {
+                temp.className = "negative";
             } else {
                 temp.className = "positive";
             }
@@ -19,46 +20,48 @@ const Accounts = () => {
         }
 
     }
-
     MakePosNeg();
 
-    return (
 
-        <div className="accounts-container">
-           <h3>Accounts</h3>
 
-            <table className="accounts-table">
-                <tr>
 
-                </tr>
-                <tr>
-                    <th>Checking Account:</th>
-                    <td className="plusmin">$5,000.00</td>
-                </tr>
-                <tr>
-                    <th>Savings Account:</th>
-                    <td className="plusmin">$80,000.00</td>
-                </tr>
-                <tr>
-                    <th>Retirement Account:</th>
-                    <td className="plusmin">$150,000.00</td>
-                </tr>
-                <tr>
-                    <th>Investments:</th>
-                    <td className="plusmin">$52,000.00</td>
-                </tr>
-                <tr>
-                    <th>Credit Cards:</th>
-                    <td className="plusmin">$14,000.00</td>
-                </tr>
-                <tr>
-                    <th>Loans:</th>
-                    <td className="plusmin">$250,000.00</td>
-                </tr>
-            </table>
+        return (
 
-        </div>
-    )
+            <div className="accounts-container">
+                <h3>Accounts</h3>
+
+                <table className="accounts-table">
+                    <tr>
+
+                    </tr>
+                    <tr>
+                        <th>Checking Account:</th>
+                        <td className="plusmin">$5,000.00</td>
+                    </tr>
+                    <tr>
+                        <th>Savings Account:</th>
+                        <td className="plusmin">$80,000.00</td>
+                    </tr>
+                    <tr>
+                        <th>Retirement Account:</th>
+                        <td className="plusmin">$150,000.00</td>
+                    </tr>
+                    <tr>
+                        <th>Investments:</th>
+                        <td className="plusmin">$52,000.00</td>
+                    </tr>
+                    <tr>
+                        <th>Credit Cards:</th>
+                        <td className="plusmin">-$14,000.00</td>
+                    </tr>
+                    <tr>
+                        <th>Loans:</th>
+                        <td className="plusmin">-$250,000.00</td>
+                    </tr>
+                </table>
+
+            </div>
+        )
 
 }
 
--- my-app/src/components/NetWorth.js
+++ my-app/src/components/NetWorth.js
@@ -3,20 +3,20 @@ import {useEffect} from "react/cjs/react.production.min";
 
 const NetWorth = () => {
 
-   function MakePosNeg() {
-            const TDs = document.querySelectorAll('.plusmin');
+    function MakePosNeg() {
+        const TDs = document.querySelectorAll('.plusmin');
 
-            for (let i = 0; i < TDs.length; i++) {
-                let temp = TDs[i];
-                if (temp.firstChild.nodeValue.indexOf('-') === 0) {
-                    temp.className = 'negative';
-                } else {
-                    temp.className = "positive";
-                }
-
+        for (let i = 0; i < TDs.length; i++) {
+            let temp = TDs[i];
+            if (temp.firstChild.nodeValue.indexOf("-") == 0) {
+                temp.className = "negative";
+            } else {
+                temp.className = "positive";
             }
 
         }
+
+    }
     MakePosNeg();
 
     return (
@@ -27,15 +27,15 @@ const NetWorth = () => {
 
                 <tr>
                     <th>Assets:</th>
-                    <td className="plusmin">$5,000.00</td>
+                    <td className="plusmin">$287,000.00</td>
                 </tr>
                 <tr>
                     <th>Debt:</th>
-                    <td className="plusmin">$80,000.00</td>
+                    <td className="plusmin">-$264,000.00</td>
                 </tr>
                 <tr>
                     <th>Net Worth:</th>
-                    <td className="plusmin">$150,000.00</td>
+                    <td className="plusmin">$23,000.00</td>
                 </tr>
             </table>
         </div>
