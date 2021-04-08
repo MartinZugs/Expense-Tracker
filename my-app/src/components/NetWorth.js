efc8009c0c7e49cac81f70e17fb59284ffdc4ccd
lahari <tnlahari@gmail.com>
2021-04-08 13:17:39 -0500

    Changes to the NetWorth component and few styling to Dashboard and account.

--- my-app/src/components/NetWorth.js
+++ my-app/src/components/NetWorth.js
@@ -7,6 +7,21 @@ const NetWorth = () => {
 
         <div className="net-worth-container">
             <h3>Net Worth</h3>
+            <table className="accounts-table">
+
+                <tr>
+                    <th>Assets:</th>
+                    <td>$5,000.00</td>
+                </tr>
+                <tr>
+                    <th>Debt:</th>
+                    <td>$80,000.00</td>
+                </tr>
+                <tr>
+                    <th>Net Worth:</th>
+                    <td>$150,000.00</td>
+                </tr>
+            </table>
         </div>
     )
 
