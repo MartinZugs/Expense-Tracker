6a56f3deff2da8faca43bab43c5c53a9ce2a0078
eethomas14 <ethomas14@luc.edu>
2021-04-10 00:12:06 -0500

    fixed some errors on the to the table on the accounts component and made accounts a class component

--- my-app/src/components/Accounts.js
+++ my-app/src/components/Accounts.js
@@ -1,69 +1,76 @@
 import React, {useEffect, useState} from "react";
 import "./account.css";
+import {Component} from "react";
 
 
 
 
-const Accounts = () => {
 
-    function MakePosNeg() {
-        const TDs = document.querySelectorAll('.plusmin');
+class Accounts extends Component {
 
-        for (let i = 0; i < TDs.length; i++) {
-            let temp = TDs[i];
-            if (temp.firstChild.nodeValue.indexOf('-') == 0) {
-                temp.className = "negative";
-            } else {
-                temp.className = "positive";
+    componentDidMount() {
+
+
+       function MakePosNeg()
+        {
+            const TDs = document.querySelectorAll('.plusmin');
+
+            for (let i = 0; i < TDs.length; i++) {
+                let temp = TDs[i];
+                if (temp.firstChild.nodeValue.indexOf('-') == 0) {
+                    temp.className = "negative";
+                } else {
+                    temp.className = "positive";
+                }
+
             }
 
         }
-
     }
 
-    window.onLoad = MakePosNeg();
 
 
-        return (
+        render (){
+            return (
 
 
-            <div className="accounts-container">
-                <h3>Accounts</h3>
+        <div className="accounts-container">
+            <h3>Accounts</h3>
 
-                <table className="accounts-table">
-                    <tr>
+            <table className="accounts-table">
 
-                    </tr>
-                    <tr>
-                        <th>Checking Account:</th>
-                        <td className="plusmin">$5,000.00</td>
-                    </tr>
-                    <tr>
-                        <th>Savings Account:</th>
-                        <td className="plusmin">$80,000.00</td>
-                    </tr>
-                    <tr>
-                        <th>Retirement Account:</th>
-                        <td className="plusmin">$150,000.00</td>
-                    </tr>
-                    <tr>
-                        <th>Investments:</th>
-                        <td className="plusmin">$52,000.00</td>
-                    </tr>
-                    <tr>
-                        <th>Credit Cards:</th>
-                        <td className="plusmin">-$14,000.00</td>
-                    </tr>
-                    <tr>
-                        <th>Loans:</th>
-                        <td className="plusmin">-$250,000.00</td>
-                    </tr>
+                <table>
+                    <tbody>
+                    <th>Checking Account:</th>
+                    <td className="plusmin">$5,000.00</td>
+                    </tbody>
+                    <tbody>
+                    <th>Savings Account:</th>
+                    <td className="plusmin">$80,000.00</td>
+                    </tbody>
+                <tbody>
+                    <th>Retirement Account:</th>
+                    <td className="plusmin">$150,000.00</td>
+                </tbody>
+                    <tbody>
+                    <th>Investments:</th>
+                    <td className="plusmin">$52,000.00</td>
+                    </tbody>
+                    <tbody>
+                    <th>Credit Cards:</th>
+                    <td className="plusmin">-$14,000.00</td>
+                    </tbody>
+                    <tbody>
+                    <th>Loans:</th>
+                    <td className="plusmin">-$250,000.00</td>
+                    </tbody>
                 </table>
+            </table>
 
-            </div>
+        </div>
 
-        )
+    )
 
-}
+}}
 
 export default Accounts;
