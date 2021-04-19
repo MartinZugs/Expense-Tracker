17c3100f63117a08b3f58e3d6dcbc659a0b06056
Marius Ebert <marius.ebert@mni.thm.de>
2021-04-19 18:38:04 -0500

    include Transaction foreign key
    change possible account types

--- backend/src/main/java/web/transaction/Transaction.java
+++ backend/src/main/java/web/transaction/Transaction.java
@@ -9,21 +9,42 @@ public class Transaction {
     double value;
     TransactionCategory category;
     boolean isBill;
+    long account_id;
 
-    public Transaction(Date date, long id, double value, TransactionCategory category, boolean isBill) {
+    public Transaction(Date date, long id, double value, TransactionCategory category, boolean isBill, long account_id) {
         this.date = date;
         this.id = id;
         this.value = value;
         this.category = category;
         this.isBill = isBill;
+        this.account_id = account_id;
     }
 
-    public Transaction(long id, double value, TransactionCategory category, boolean isBill) {
+    public Transaction(Date date, long id, double value, boolean isBill, long account_id) {
+        this.date = date;
+        this.id = id;
+        this.value = value;
+        this.category = TransactionCategory.OTHER;
+        this.isBill = isBill;
+        this.account_id = account_id;
+    }
+
+    public Transaction(long id, double value, TransactionCategory category, boolean isBill, long account_id) {
         this.date = new Date();
         this.id = id;
         this.value = value;
         this.category = category;
         this.isBill = isBill;
+        this.account_id = account_id;
+    }
+
+    public Transaction(long id, double value, boolean isBill, long account_id) {
+        this.date = new Date();
+        this.id = id;
+        this.value = value;
+        this.category = TransactionCategory.OTHER;
+        this.isBill = isBill;
+        this.account_id = account_id;
     }
 
     public Transaction() {}
@@ -47,5 +68,9 @@ public class Transaction {
 
     public boolean getIsBill() {
         return isBill;
+    }
+
+    public long getAccount_id() {
+        return account_id;
     }
 }
